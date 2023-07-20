0244dd46574cb6e26fc98f977e890123a35b694e
johndward01 <johndward01@gmail.com>
2023-07-20 14:37:47 -0500

    Add project files.

---
+++ Factory_Pattern_Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿// TODO: Create a static class called LoggerFactory
+// TODO: Create an Interface called ICustomLogger
+// TODO: Inside of the interface, create a WriteLog() method
+// TODO: Create a class called FileLogger that implements the ICustomLogger interface
+// TODO: Create a class called ConsoleLogger that implements the ICustomLogger interface
+// Bonus: Create a class called DatabaseLogger that implements the ICustomLogger interface
+
+
+// TODO: Inside of the LoggerFactory class, create a method with a string parameter
+//       that returns the correct instance of an ICustomLogger 
+
+// TODO: Ask the user what type of logger they would like to use, store their answer,
+//       use the LoggerFactory to create the right instance, and then write 1 log creatively
