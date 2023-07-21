bbcac02e1378ba3d1b540b005bc59268ecb2bcca
johndward01 <johndward01@gmail.com>
2023-07-21 09:41:05 -0500

    update

---
+++ Factory_Pattern_Exercise/ConsoleLogger.cs
@@ -0,0 +1,34 @@
+﻿namespace Factory_Pattern_Exercise;
+
+public class ConsoleLogger : ICustomLogger
+{
+    public void Critical()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Debug()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Error()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Information()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Trace()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Warning()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Factory_Pattern_Exercise/Program.cs
+++ Factory_Pattern_Exercise/Program.cs
@@ -4,17 +4,19 @@
 
 // TODO: Create an Interface called ICustomLogger
 // TODO: Inside of the interface, create 6 methods
-//          LOG LEVELS
-//          - Trace();
-//          - Debug();
-//          - Information();
-//          - Warning();
-//          - Error();
-//          - Critical();
+//                  LOG LEVELS
+//          - Trace(string logMessage);
+//          - Debug(string logMessage);
+//          - Information(string logMessage);
+//          - Warning(string logMessage);
+//          - Error(string logMessage);
+//          - Critical(string logMessage);
 
-// TODO: Create a class called FileLogger that implements the ICustomLogger interface
+// TODO: Use this Template for your methods (Log Message Template: Log Level Name, Date/Time, logMessage)
+// TODO: Create a class called FileLogger that implements the ICustomLogger interface 
 // TODO: Create a class called ConsoleLogger that implements the ICustomLogger interface
 // Bonus: Create a class called DatabaseLogger that implements the ICustomLogger interface
+
 
 
 
