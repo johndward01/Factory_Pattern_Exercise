672e527f5c32836a8032306d1675523a8a311c69
johndward01 <johndward01@gmail.com>
2023-07-21 09:41:19 -0500

    update

--- Factory_Pattern_Exercise/Program.cs
+++ Factory_Pattern_Exercise/Program.cs
@@ -1,20 +1,22 @@
 ﻿// TODO: Create a static class called LoggerFactory
-// TODO: Inside of the LoggerFactory class, create a method with a string parameter
+// TODO: Inside of the LoggerFactory class, create a method called CreateLogger() with a string parameter
 //       that returns the correct instance of an ICustomLogger 
 
 // TODO: Create an Interface called ICustomLogger
 // TODO: Inside of the interface, create 6 methods
-//          LOG LEVELS
-//          - Trace();
-//          - Debug();
-//          - Information();
-//          - Warning();
-//          - Error();
-//          - Critical();
+//                  LOG LEVELS
+//          - Trace(string logMessage);
+//          - Debug(string logMessage);
+//          - Information(string logMessage);
+//          - Warning(string logMessage);
+//          - Error(string logMessage);
+//          - Critical(string logMessage);
 
-// TODO: Create a class called FileLogger that implements the ICustomLogger interface
+// TODO: Use this Template for your methods (Log Message Template: Log Level Name, Date/Time, logMessage)
+// TODO: Create a class called FileLogger that implements the ICustomLogger interface 
 // TODO: Create a class called ConsoleLogger that implements the ICustomLogger interface
 // Bonus: Create a class called DatabaseLogger that implements the ICustomLogger interface
+
 
 
 
