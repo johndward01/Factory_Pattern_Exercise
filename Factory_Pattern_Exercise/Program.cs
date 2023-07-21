4d62e5505a256ed7ed2fb3c77102d8f1e4bbf95f
johndward01 <johndward01@gmail.com>
2023-07-21 11:32:36 -0500

    update

--- Factory_Pattern_Exercise/Program.cs
+++ Factory_Pattern_Exercise/Program.cs
@@ -13,11 +13,11 @@
 //          - Critical(string logMessage);
 
 // TODO: Use this Template for your methods (Log Message Template: Log Level Name, Date/Time, logMessage)
-// TODO: Create a class called FileLogger that implements the ICustomLogger interface 
 // TODO: Create a class called ConsoleLogger that implements the ICustomLogger interface
-// Bonus: Create a class called DatabaseLogger that implements the ICustomLogger interface
-
-
+// TODO: Create a class called FileLogger that implements the ICustomLogger interface 
+//       (Create a file in the root of your project called LogFile.txt, right-click the file, copy the path, and use that with the File.AppendAllText() method)
+// Bonus: Create a class called DatabaseLogger that implements the ICustomLogger interface 
+//        (Use the dapper exercise to help you implement this)
 
 
 // TODO: Use the LoggerFactory to create each type of custom logger and then write 1 log foreach of the LOG LEVELS creatively
